73050eda301c5a724fe370aad5737515c63743d9
wrgalvao <wrgalvao@unifesspa.edu.br>
2022-08-13 18:14:41 -0300

    Adicionada a parte 1 de Condições em JavaScript

---
+++ condicoes/parte1.js
@@ -0,0 +1,11 @@
+var velocidadePermitida = 60
+var velocidade = 70
+if(velocidade > 60)
+{
+    console.log(`Voce ultrapassou a velocidade permitida de ${velocidadePermitida} km/h você foi multado`)
+    console.log('Dirija sempre utilizando o cinto de segunrança!')
+}
+else
+{
+    console.log(`Continue sua viagem tranquilamente e mantenha-se na velocidade maxima permitida de ${velocidadePermitida} km/h`)
+}
