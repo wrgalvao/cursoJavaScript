ddb53423faed7c4ce9de7af15f37df6bd151b1b8
wrgalvao <wrgalvao@unifesspa.edu.br>
2022-08-14 16:01:28 -0300

    Adição de funções em JS

---
+++ funcoes/funcoes.js
@@ -0,0 +1,26 @@
+function soma(valor, valor2)
+{
+    var soma = valor + valor2
+    return soma
+}
+
+result = parimp(5)
+console.log(result)
+function parimp(n)
+{
+    if(n%2 == 0)
+    {
+        return 'par'
+    }
+    else
+    {
+        return 'impar'
+    }
+}
+
+var soma = function(n1 , n2)
+{
+    return n1+n2
+}
+console.log(soma(1,2))
+
