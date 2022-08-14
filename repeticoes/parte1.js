05c950b338a74ba7ea0dde09389746a5fb6ecd6c
wrgalvao <wrgalvao@unifesspa.edu.br>
2022-08-14 10:55:54 -0300

    ADIÇÃO DAS ESTRUTURAS DE REPETIÇÃO

---
+++ repeticoes/parte1.js
@@ -0,0 +1,27 @@
+//CRIAÇÃO DE VARIÁVEIS
+var valor = 10
+var contador = 0
+
+// ESTRUTURA DE REPETIÇÃO WHILE
+console.log('ESTRUTURA WHILE')
+while(contador < valor)
+{
+    console.log(contador)
+    contador++
+}
+
+// ESTRUTURA DE REPETIÇÃO DO WHILE
+console.log('ESTRUTURA DO WHILE')
+contador = 0
+do
+{
+    console.log(contador)
+    contador++
+}while(contador < valor)
+
+//ESTRUTURA DE REPETIÇÃO FOR
+console.log('ESTRUTURA FOR')
+for(contador = 0;contador < valor;contador++)
+{
+    console.log(contador)
+}
